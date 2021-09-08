92ec4e2ad7fd5ebc88bf75989a6d859dc3c20c01
Tom Hohl <tom.hohl@gmail.com>
2021-09-07 18:24:42 -0600

    home styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import "./Home.css"
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home screen</h1>
-            <h5>This home screen is presently useless. But it's here! And it works! Maybe there'd be info or other pathway options here.</h5>
+        <div className="card-wrapper">
+            <p className="card1">This home screen is presently useless. But it's here! And it works! Maybe there'd be info or other pathway options here.</p>
+            <p className="card2">Perhaps some sort of description about how the app works or how great it is. Glowing reviews from obscure and untrustworthy sources.</p>
+            <p className="card3">Maybe some sort of trendy animated thing making the app look really fleshed out. A bit of wow factor. Something to really make an impression.</p>
         </div>
     )
 }
